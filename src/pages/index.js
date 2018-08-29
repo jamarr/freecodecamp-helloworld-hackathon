a768abda4aadd2b52690cd0168323d69150f9855
Jamarr Edwards <jamarredwards@gmail.com>
2018-08-28 23:14:03 -0700

    updated readme, adjusted paragrah, and font
    removed gatsby defualt readme. fixed typo.

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,9 +4,10 @@ import Link from 'gatsby-link'
 const IndexPage = () => (
   <div>
     <h3>This Jamarr Edwards</h3>
-   <p>I am a FreeCodeCamp user located in San Francisco.  <br/>
+   <p>I am a FreeCodeCamp user located in San Francisco.  <br />
    who enjoys learning Javascript, Java "Go Android!!", Kotlin, and more. <br/>
-   And I would love to participate in your hackathon.</p>
+   And I would love to participate in your hackathon. <br />	
+   </p>
 			    
 
   </div>
