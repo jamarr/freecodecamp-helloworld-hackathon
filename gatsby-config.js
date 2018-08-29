b9688c45e2aec5bad37a9830ffef7bc060077339
Jamarr Edwards <jamarredwards@gmail.com>
2018-08-28 20:49:50 -0700

    first commit

---
+++ gatsby-config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  siteMetadata: {
+    title: 'FreeCodeCamp Hackathon',
+  },
+  plugins: ['gatsby-plugin-react-helmet'],
+}
---
+++ src/pages/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const IndexPage = () => (
+  <div>
+    <h3>This Jamarr Edwards</h3>
+   <p>I am a FreeCodeCamp user located in San Francisco.  <br/>
+   who enjoys learning Javascript, Java "Go Android!!", Kotlin, and more. <br/>
+   And I would love to participate in your hackathon.</p>
+			    
+
+  </div>
+)
+
+export default IndexPage
