ecbe512958e574615959378332b3a2a29b2e8720
Jamarr Edwards <jamarredwards@gmail.com>
2018-08-29 20:03:44 -0700

    Fix typo

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,7 @@ import Link from 'gatsby-link'
 
 const IndexPage = () => (
   <div>
-    <h3>This Jamarr Edwards</h3>
+    <h3>Hello, My name is Jamarr Edwards</h3>
    <p>I am a FreeCodeCamp user located in San Francisco.  <br />
    who enjoys learning Javascript, Java "Go Android!!", Kotlin, and more. <br/>
    And I would love to participate in your hackathon. <br />	
